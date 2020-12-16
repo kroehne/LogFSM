9977118a7a8aa9e21b8081ca900b4d3511588ab4
kroehne <kroehne@dipf.de>
2020-12-16 15:20:52 +0100

    Update update.R
    0.4.5.11

--- www/update.R
+++ www/update.R
@@ -1,6 +1,6 @@
 rmarkdown::render_site(encoding = 'UTF-8', input="www")
 
-current_version <- '0.4.5.10'
+current_version <- '0.4.5.11'
   
 install_script <- c(paste0("cat('LogFSM - Install Version ",current_version, "\\n\\n')"),
                     paste0("current_version <- '",current_version,"'"),
