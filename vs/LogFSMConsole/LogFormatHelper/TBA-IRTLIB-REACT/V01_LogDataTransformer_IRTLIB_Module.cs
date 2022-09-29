83a31c62b785753eed06e8a580ecc2093fd18e23
Ulf Kröhne <kroehne@dipf.de>
2022-09-29 16:11:09 +0200

    OpenSSL Bug

--- vs/LogFSMConsole/LogFormatHelper/TBA-IRTLIB-REACT/V01_LogDataTransformer_IRTLIB_Module.cs
+++ vs/LogFSMConsole/LogFormatHelper/TBA-IRTLIB-REACT/V01_LogDataTransformer_IRTLIB_Module.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using Ionic.Zip;
