5b96cdd107ff7dac14db51e73c6692e6dbc94f54
Ulf Kröhne <ulf@kroehne.de>
2023-12-11 16:48:51 +0100

    prepare 0.4.5.31

--- r/R/zzz.R
+++ r/R/zzz.R
@@ -11,15 +11,21 @@ setPathVariable <-  function(pkgname){
   osname <- Sys.info()["sysname"]
   ostype <- .Platform$OS.type
   is64 <- Sys.info()[["machine"]] == "x86_64" || Sys.info()[["machine"]] == "x86-64"
+  isarm <- Sys.info()[["machine"]] == "arm64" || Sys.info()[["machine"]] == "arm"
 
   logFSMWorkerFile <<- "LogFSMConsole.exe"
   logFSMworkerPath <<- file.path(system.file(package=pkgname), 'extdata/win-x64')
 
   if (osname == "Darwin"){  # MacOS (64 bit)
-    logFSMWorkerFile <<- "LogFSMConsole"
-    logFSMworkerPath <<- file.path(system.file(package=pkgname), 'extdata/osx-x64')
-  }
-  else if (ostype == "unix"){ # Linux (64 bit)
+    if (isarm){
+      logFSMWorkerFile <<- "LogFSMConsole"
+      logFSMworkerPath <<- file.path(system.file(package=pkgname), 'extdata/osx-arm64')
+    } else {
+      logFSMWorkerFile <<- "LogFSMConsole"
+      logFSMworkerPath <<- file.path(system.file(package=pkgname), 'extdata/osx-x64')
+    }
+
+  } else if (ostype == "unix"){ # Linux (64 bit)
     logFSMWorkerFile <<- "LogFSMConsole"
     logFSMworkerPath <<- file.path(system.file(package=pkgname), 'extdata/linux-x64')
   }
